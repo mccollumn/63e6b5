de9b6acc7d27be32be8dd0122351cd2dc08e1ba2
Nick McCollum <nmccollum81@gmail.com>
2025-05-25 15:29:43 -0700

    Removed babel - not compatibile with Turbopack

--- jest.config.ts
+++ jest.config.ts
@@ -191,12 +191,6 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-  },
-  // transform: {
-  //   "^.+\\.tsx?$": "babel-jest",
-  // },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
